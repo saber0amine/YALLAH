e2a41f42966343b1e66e654340e03fd84f659881
saber0amine <120688951+saber0amine@users.noreply.github.com>
2024-04-10 03:35:26 +0000

    resolv push

---
+++ src/main/java/com/project/yallah/YallahApplication.java
@@ -0,0 +1,13 @@
+package com.project.yallah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YallahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/yallah/YallahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.yallah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
